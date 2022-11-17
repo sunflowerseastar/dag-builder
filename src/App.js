90430c5b8f0579812c54a11fb67b8eea73af6fcb
Grant Surlyn <grant@sunflowerseastar.com>
2022-11-17 16:00:42 -0500

    add arrows, layering, spacing, and more data

--- src/App.js
+++ src/App.js
@@ -2,60 +2,125 @@ import { useState } from "react";
 import "./style.css";
 
 import Dag from "./Dag.js";
-import data from "./data.json";
+
+import dag1 from "./data/dag.json";
+import diamond from "./data/diamond.json";
+import en from "./data/en.json";
+import ex from "./data/ex.json";
+import grafo from "./data/grafo.json";
+
+const SelectionGroup = ({ heading, options, selection, setSelection }) => (
+  <div>
+    <p>{heading}:</p>
+    {options.map((option) => (
+      <a
+        key={option.name}
+        value={option.name}
+        className={selection === option.name ? "active" : ""}
+        onClick={() => setSelection(option.name)}
+      >
+        {option.name}
+      </a>
+    ))}
+  </div>
+);
 
 function App() {
+  const spacingChoices = [{ name: "Default" }, { name: "Compact" }];
+  const [selectedSpacing, setSelectedSpacing] = useState(
+    spacingChoices[0].name
+  );
+  const currentSpacing = spacingChoices.find(
+    ({ name }) => name === selectedSpacing
+  );
+
   const splineChoices = [
-    { name: "Linear", spline: window.d3.curveLinear },
-    { name: "Catmull-Rom", spline: window.d3.curveCatmullRom },
-    { name: "Step Before", spline: window.d3.curveStepBefore },
+    { name: "Linear", spline: window.d3.curveLinear, areArrowsShown: true },
+    {
+      name: "Catmull-Rom",
+      spline: window.d3.curveCatmullRom,
+      areArrowsShown: true,
+    },
+    {
+      name: "Step Before",
+      spline: window.d3.curveStepBefore,
+      areArrowsShown: false,
+    },
   ];
   const [selectedSpline, setSelectedSpline] = useState(splineChoices[0].name);
   const currentSpline = splineChoices.find(
     ({ name }) => name === selectedSpline
   );
 
+  const layeringChoices = [
+    {
+      name: "Simplex",
+      layering: window.d3.layeringSimplex(),
+    },
+    {
+      name: "LongestPath",
+      layering: window.d3.layeringLongestPath(),
+    },
+    {
+      name: "Coffman-Graham",
+      layering: window.d3.layeringCoffmanGraham(),
+    },
+  ];
+  const [selectedLayering, setSelectedLayering] = useState(
+    layeringChoices[0].name
+  );
+  const currentLayering = layeringChoices.find(
+    ({ name }) => name === selectedLayering
+  );
+
   const dataChoices = [
-    { name: "data1", data: data},
-    { name: "data2", data: data},
+    { name: "example 2", data: en },
+    { name: "example 3", data: diamond },
+    { name: "example 4", data: dag1 },
+    { name: "example 5", data: ex },
+    { name: "example 6", data: grafo },
   ];
   const [selectedData, setSelectedData] = useState(dataChoices[0].name);
-  const currentData = dataChoices.find(
-    ({ name }) => name === selectedData
-  );
+  const currentData = dataChoices.find(({ name }) => name === selectedData);
 
   return (
     <div className="app">
       <header>
-        <div>
-        <p>Spline:</p>
-        <select
-          value={selectedSpline}
-          onChange={(e) => setSelectedSpline(e.target.value)}
-        >
-          {splineChoices.map((x) => (
-            <option key={x.name} value={x.name}>
-              {x.name}
-            </option>
-          ))}
-        </select>
-        </div>
-        <div>
-        <p>Data:</p>
-        <select
-          value={selectedData}
-          onChange={(e) => setSelectedData(e.target.value)}
-        >
-          {dataChoices.map((x) => (
-            <option key={x.name} value={x.name}>
-              {x.name}
-            </option>
-          ))}
-        </select>
+        <div className="header-inner">
+          <SelectionGroup
+            heading="spline"
+            options={splineChoices}
+            selection={selectedSpline}
+            setSelection={setSelectedSpline}
+          />
+          <SelectionGroup
+            heading="spacing"
+            options={spacingChoices}
+            selection={selectedSpacing}
+            setSelection={setSelectedSpacing}
+          />
+          <SelectionGroup
+            heading="layering"
+            options={layeringChoices}
+            selection={selectedLayering}
+            setSelection={setSelectedLayering}
+          />
+          <SelectionGroup
+            heading="dataset"
+            options={dataChoices}
+            selection={selectedData}
+            setSelection={setSelectedData}
+          />
         </div>
       </header>
 
-      <Dag spline={currentSpline.spline} data={data}></Dag>
+      <Dag
+        areArrowsShown={currentSpline.areArrowsShown}
+        data={currentData.data}
+        layering={currentLayering.layering}
+        spacing={currentSpacing.name}
+        spline={currentSpline.spline}
+      ></Dag>
     </div>
   );
 }
--- src/Dag.js
+++ src/Dag.js
@@ -5,14 +5,25 @@ import { useEffect } from "react";
 
 const d3 = window.d3;
 
-const Dag = ({data, spline }) => {
+const Dag = ({ areArrowsShown, data, layering, spacing, spline }) => {
   useEffect(() => {
     const dag = d3.dagStratify()(data);
-    const nodeRadius = 20;
+    const nodeRadius = 30;
+    const nodeSpacingDefault = () => [
+      1.2 * nodeRadius * 2 * 1.5,
+      nodeRadius * 2 * 1.5,
+    ];
+    const nodeSpacingCompact = (node) => [
+      (node ? 3.6 : 0.25) * nodeRadius,
+      3 * nodeRadius,
+    ];
     const layout = d3
       .sugiyama() // base layout
+      .layering(layering)
       .decross(d3.decrossOpt()) // minimize number of crossings
-      .nodeSize((node) => [(node ? 3.6 : 0.25) * nodeRadius, 3 * nodeRadius]); // set node size instead of constraining to fit
+      .nodeSize(
+        spacing === "Compact" ? nodeSpacingCompact : nodeSpacingDefault
+      );
     const { width, height } = layout(dag);
 
     // --------------------------------
@@ -22,13 +33,6 @@ const Dag = ({data, spline }) => {
     svgSelection.attr("viewBox", [0, 0, width, height].join(" "));
     const defs = svgSelection.append("defs"); // For gradients
 
-    const steps = dag.size();
-    const interp = d3.interpolateRainbow;
-    const colorMap = new Map();
-    for (const [i, node] of dag.idescendants().entries()) {
-      colorMap.set(node.data.id, interp(i / steps));
-    }
-
     // How to draw edges
     const line = d3
       .line()
@@ -37,6 +41,7 @@ const Dag = ({data, spline }) => {
       .y((d) => d.y);
 
     // clean-up (for re-renders)
+    svgSelection.selectAll("g").remove();
     svgSelection.selectAll("path").remove();
 
     // Plot edges
@@ -62,14 +67,11 @@ const Dag = ({data, spline }) => {
           .attr("x2", target.x)
           .attr("y1", source.y)
           .attr("y2", target.y);
-        grad
-          .append("stop")
-          .attr("offset", "0%")
-          .attr("stop-color", colorMap.get(source.data.id));
+        grad.append("stop").attr("offset", "0%").attr("stop-color", "#4e3e80");
         grad
           .append("stop")
           .attr("offset", "100%")
-          .attr("stop-color", colorMap.get(target.data.id));
+          .attr("stop-color", "#1a0d36");
         return `url(#${gradId})`;
       });
 
@@ -83,21 +85,49 @@ const Dag = ({data, spline }) => {
       .attr("transform", ({ x, y }) => `translate(${x}, ${y})`);
 
     // Plot node circles
-    nodes
-      .append("circle")
-      .attr("r", nodeRadius)
-      .attr("fill", (n) => colorMap.get(n.data.id));
+    nodes.append("circle").attr("r", nodeRadius).attr("fill", "#4e3e80");
+
+    if (areArrowsShown) {
+      const arrowSize = (nodeRadius * nodeRadius) / 20.0;
+      const arrowLen = Math.sqrt((4 * arrowSize) / Math.sqrt(3));
+      const arrow = d3.symbol().type(d3.symbolTriangle).size(arrowSize);
+      svgSelection
+        .append("g")
+        .selectAll("path")
+        .data(dag.links())
+        .enter()
+        .append("path")
+        .attr("d", arrow)
+        .attr("transform", ({ source, target, points }) => {
+          const [end, start] = points.slice().reverse();
+          // This sets the arrows the node radius (20) + a little bit (3) away from the node center, on the last line segment of the edge. This means that edges that only span ine level will work perfectly, but if the edge bends, this will be a little off.
+          const dx = start.x - end.x;
+          const dy = start.y - end.y;
+          const scale = (nodeRadius * 1.15) / Math.sqrt(dx * dx + dy * dy);
+          // This is the angle of the last line segment
+          const angle = (Math.atan2(-dy, -dx) * 180) / Math.PI + 90;
+          return `translate(${end.x + dx * scale}, ${
+            end.y + dy * scale
+          }) rotate(${angle})`;
+        })
+        // .attr("fill", ({ target }) => colorMap[target.data.id])
+        .attr("fill", "#4e3e80")
+        .attr("stroke", "white")
+        .attr("stroke-width", 1)
+        .attr("stroke-dasharray", `${arrowLen},${arrowLen}`);
+    }
 
     // Add text to nodes
     nodes
       .append("text")
       .text((d) => d.data.id)
+      .style("dominant-baseline", "central")
       .attr("font-weight", "bold")
       .attr("font-family", "sans-serif")
       .attr("text-anchor", "middle")
       .attr("alignment-baseline", "middle")
       .attr("fill", "white");
-  }, [spline]);
+  }, [data, layering, spacing, spline]);
   return <svg className="dag-svg"></svg>;
 };
 
